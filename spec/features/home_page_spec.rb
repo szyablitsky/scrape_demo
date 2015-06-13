54009b1f34140cf97b72459cedf3fb376b617b32
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2015-06-13 21:53:16 +1000

    form layout

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature 'On home page', type: :feature do
   scenario 'User sees IMEI prompt' do
     visit '/'
-    expect(page).to have_text 'Enter IMEI'
+    expect(page).to have_content 'Scrape Demo'
   end
 end
